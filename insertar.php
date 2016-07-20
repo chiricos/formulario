32136664790685348f72d85ff5ff86edb35a12fd
chiricos <edwarddiaz92@gmail.com>
2016-07-20 18:18:41 -0500

    cambios

--- insertar.php
+++ insertar.php
@@ -1,9 +1,14 @@
 <?php
 include('conexion.php');
-
+if(
+    isset($_POST['fecha']) && !empty($_POST['fecha']) &&
+    isset($_POST['municipio']) && !empty($_POST['municipio'])
+){
     $con = mysql_connect($hots,$user,$pw) or die("problemas al conectar al localhost");
     mysql_select_db($db,$con) or die("problema al conectar con la base de datos");
-    mysql_query($con,"INSERT INTO datos (id,fecha,municipio) VALUES (1,'fdsa','fdsa')");
+    mysql_query("INSERT INTO datos (fecha,municipio) VALUES ('$_POST[fecha]','$_POST[municipio]')",$con);
     echo "datos insertados";
-
+}else{
+    echo "problemas al insertar datos";
+}
 ?>
